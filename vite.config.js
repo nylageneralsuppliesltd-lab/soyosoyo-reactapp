0a666f9b34078079443e83042d1bc9bbbbacc7a5
nylageneralsuppliesltd-lab <nylageneralsuppliesltd@gmail.com>
2026-01-11 00:22:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-<<<<<<< HEAD
+
     base: '/',
 
-=======
-  base: mode === 'development' ? '/' : '/soyosoyo-reactapp/', // GitHub Pages
->>>>>>> 8cd1a6e (Update dashboard routing and Vite config for GitHub Pages)
   plugins: [react()],
   resolve: {
     alias: {
