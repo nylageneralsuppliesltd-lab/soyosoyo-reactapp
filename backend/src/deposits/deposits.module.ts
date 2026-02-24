4289b9a008696d7fe7372bc2416fa508a3492b45
nylageneralsuppliesltd-lab <nylageneralsuppliesltd@gmail.com>
2026-02-24 21:46:07 +0300

    fix(backend): import RepaymentsModule into DepositsModule

--- backend/src/deposits/deposits.module.ts
+++ backend/src/deposits/deposits.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { DepositsService } from './deposits.service';
 import { DepositsController } from './deposits.controller';
 import { PrismaModule } from '../prisma.module';
-import { AuditModule } from '../audit/audit.module';
+import { RepaymentsModule } from '../repayments/repayments.module';
 
 @Module({
-  imports: [PrismaModule, AuditModule],
+  imports: [PrismaModule, RepaymentsModule],
   controllers: [DepositsController],
   providers: [DepositsService],
   exports: [DepositsService],
